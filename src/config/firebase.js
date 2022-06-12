30911d55f509573414e6f7fa7e2f5b97b6854f68
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-06-11 21:33:03 -0500

    fix: solution heroku deploy with runtimeEnv

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,20 +1,23 @@
 // Import the functions you need from the SDKs you need
+import runtimeEnv from "@mars/heroku-js-runtime-env";
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+
+const env = runtimeEnv();
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: process.env.REACT_APP_FIREBASE_APP_ID,
-	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
