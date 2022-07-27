72ca3aebe6b7ec9efffd53d89d7a2779a0fa5201
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-07-27 00:49:55 -0500

    fix: all endpoints

--- src/app/endpoints/apis.js
+++ src/app/endpoints/apis.js
@@ -1,5 +1,6 @@
 import runtimeEnv from "@mars/heroku-js-runtime-env";
 const env = runtimeEnv();
 
-export const API_JOBS = "https://www.themuse.com/api/public/jobs?page=1";
 export const API_BACKEND = env.REACT_APP_BACKEND || "http://localhost:3001/api";
+export const API_USERS = `${API_BACKEND}/users`;
+export const API_JOBS = `${API_BACKEND}/jobs`;
