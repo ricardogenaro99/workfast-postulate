8175766ad66c0084637005080df289dd59ae157f
Ricardo Genaro <genaro.choquehuanca.palli@gmail.com>
2022-08-06 20:38:49 -0500

    fix: environment

--- src/config/firebase.js
+++ src/config/firebase.js
@@ -1,23 +1,24 @@
-// Import the functions you need from the SDKs you need
-import runtimeEnv from "@mars/heroku-js-runtime-env";
 import { getAnalytics } from "firebase/analytics";
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import {
+	REACT_APP_FIREBASE_API_KEY,
+	REACT_APP_FIREBASE_APP_ID,
+	REACT_APP_FIREBASE_AUTH_DOMAIN,
+	REACT_APP_FIREBASE_MEASUREMENT_ID,
+	REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	REACT_APP_FIREBASE_PROJECT_ID,
+	REACT_APP_FIREBASE_STORAGE_BUCKET
+} from "../environment";
 
-const env = runtimeEnv();
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-	apiKey: env.REACT_APP_FIREBASE_API_KEY,
-	authDomain: env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-	projectId: env.REACT_APP_FIREBASE_PROJECT_ID,
-	storageBucket: env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-	messagingSenderId: env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-	appId: env.REACT_APP_FIREBASE_APP_ID,
-	measurementId: env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+	apiKey: REACT_APP_FIREBASE_API_KEY,
+	authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+	projectId: REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: REACT_APP_FIREBASE_APP_ID,
+	measurementId: REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
--- src/modules/AppModule.js
+++ src/modules/AppModule.js
@@ -1,13 +1,11 @@
-import runtimeEnv from "@mars/heroku-js-runtime-env";
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
 import styled from "styled-components";
 import Main from "../app/components/Main";
 import { Aside } from "../app/shared/components";
+import { REACT_APP_STRIPE_API_KEY } from "../environment";
 
-const env = runtimeEnv();
-
-const stripePromise = loadStripe(env.REACT_APP_STRIPE_API_KEY);
+const stripePromise = loadStripe(REACT_APP_STRIPE_API_KEY);
 
 const Container = styled.div`
 	width: 100%;
